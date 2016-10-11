841ac0630e5f39566b508fdfa07681616480fc9c
MattyChance <chenmatty.wang@gmail.com>
2016-10-11 03:53:19 +0000

    started hangman game

---
+++ hangman.js
@@ -0,0 +1,19 @@
+var wordList = ['apple', 'banana', 'mokey', 'conversation',
+     'dream', 'stupidity', 'solution', 'rainbow', 'adventrue', 'goodness'
+ ];
+
+function guessWord() {
+     var theWord = wordList[Math.floor(Math.random() * 10)];
+
+ }
+
+
+console.log('\n' + '|' + '\n' + '|' + "___________");
+    // _________
+    // |         |
+    // |         0
+    // |        /|\
+    // |        / \
+    // |
+    // |
+    //    
