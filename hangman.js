9255f9c012e736c5766bb8964f3fdb3275162b23
MattyChance <chenmatty.wang@gmail.com>
2016-10-12 01:03:19 +0000

    updates on hangman

--- hangman.js
+++ hangman.js
@@ -1,14 +1,53 @@
+//myhandman game
+//step one: created a list of word and the base of hangman
+//step two: function to guess the word
+//'______'
+//
+
 var wordList = ['apple', 'banana', 'mokey', 'conversation',
-     'dream', 'stupidity', 'solution', 'rainbow', 'adventrue', 'goodness'
- ];
+    'dream', 'stupidity', 'solution', 'rainbow', 'adventrue', 'goodness'
+];
+
+var prompt = require("prompt");
+
+var base =
+    `     _________
+    |         |
+    |         0
+    |        /|\\
+    |        / \\
+    |
+    |
+    ---------------
+    `;
+
+
+// console.log(base);
 
 function guessWord() {
-     var theWord = wordList[Math.floor(Math.random() * 10)];
+    var theWord = wordList[Math.floor(Math.random() * 10)];
+    var reset = 
+    `    ---------
+    |
+    |
+    |
+    |
+    |
+    |
+    ------------
+    `;
+     
+    prompt.get('Guess a word; Save the man', function () {
+        console.log(reset);
+    });
+    
+}
 
- }
+guessWord();
 
 
-console.log('\n' + '|' + '\n' + '|' + "___________");
+
+// console.log('\n' + '|' + '\n' + '|' + "___________");
     // _________
     // |         |
     // |         0
@@ -16,4 +55,4 @@ console.log('\n' + '|' + '\n' + '|' + "___________");
     // |        / \
     // |
     // |
-    //    
+    // ------------   
