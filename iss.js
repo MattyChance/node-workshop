c3954b595d100817fd0ad38b47b14a64b5dd24a4
MattyChance <chenmatty.wang@gmail.com>
2016-10-12 17:34:33 +0000

    updates small

--- iss.js
+++ iss.js
@@ -4,12 +4,13 @@ request('http://api.open-notify.org/iss-now.json', function (err, result) {
     if (err) {
         console.log(err);
     } else {
-        console.log(result);
         var parsedData = JSON.parse(result.body);
-        var latitude = parsedData.iss_position.latitude;
-        var longitude = parsedData.iss_position.longitude;
+        console.log(result.body);
+
+        // var latitude = parsedData.iss_position.latitude;
+        // var longitude = parsedData.iss_position.longitude;
         
-        console.log('Latitude: ' + latitude.toFixed(2) + '\n' + 'Longitude: ' + longitude.toFixed(2));
+        // console.log('Latitude: ' + latitude.toFixed(2) + '\n' + 'Longitude: ' + longitude.toFixed(2));
     }
 });
 
